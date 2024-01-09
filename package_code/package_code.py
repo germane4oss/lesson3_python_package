04d52bcea244bdeb8c7a817c800bf28a86529a8a
German Silva <german@e4oss.ca>
2024-01-09 05:53:55 -0500

    Initial commit for Lesson 3 Package

---
+++ package_code/package_code.py
@@ -0,0 +1,12 @@
+#
+
+def calc_average(a: float, b: float) -> float:
+    '''  '''
+    return (a + b) / 2
+
+def calc_min(a: float, b: float) -> float:
+    '''  '''
+    if (a <= b):
+        return a
+    else:
+        return b
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='lesson3_package',
+    version='0.1.0',
+    author='German Silva',
+    author_email='gsilva2k5@gmail.com',
+    packages=['package_code'],
+)
