b32fd8d024651ccfd2e9bb0f6b91b4adf4a0f3b8
German Silva <152159251+germane4oss@users.noreply.github.com>
2024-01-09 11:53:34 -0500

    Update package_code.py

--- package_code/package_code.py
+++ package_code/package_code.py
@@ -1,11 +1,12 @@
-#
+# Function to calculate the average between two operands
 
 def calc_average(a: float, b: float) -> float:
-    '''  '''
+    ''' calculate the average '''
     return (a + b) / 2
 
+# Function to calculate the minimum between two operands
 def calc_min(a: float, b: float) -> float:
-    '''  '''
+    ''' calculate the minimum '''
     if (a <= b):
         return a
     else:
